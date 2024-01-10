1aa92a098bbcab05f79450921ad42fbe9db0fe55
syc-xiaosu03 <syc_kblk@126.com>
2024-01-11 05:31:45 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	title: "sansu-blog",
 	// 以下两项base&dest, 只在部署时开启设置, 开发时需注释掉(为默认值不设定!!!)
-	// base: "./", // //https://github.com/syc-xiaosu03/ 下的静态资源访问路径
-	// dest: "./dist", // 设置打包输出目录
+	base: "./", // //https://github.com/syc-xiaosu03/ 下的静态资源访问路径
+	dest: "./dist", // 设置打包输出目录
 	description: "Just playing around",
 	description: "技术博客",
 	head: [["link", { rel: "icon", href: "/logo.png" }]],
